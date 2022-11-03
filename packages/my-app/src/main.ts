76a41edd3cbfac3ee7a0d84ed884081b30747ac8
krong <tairong_kang@innodisk.com>
2022-11-03 15:39:07 +0800

    feat: update sample

--- packages/my-app/src/main.ts
+++ packages/my-app/src/main.ts
@@ -5,13 +5,14 @@
 
 import * as express from 'express';
 import * as path from 'path';
+import { utils } from '@myorg/utils';
 
 const app = express();
 
 app.use('/assets', express.static(path.join(__dirname, 'assets')));
 
 app.get('/api', (req, res) => {
-  res.send({ message: 'Welcome to my-app!' });
+  res.send({ message: `用到nx建立的share lib: ${utils()}` });
 });
 
 const port = process.env.port || 3333;
