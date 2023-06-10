5a54bea511d4c0d4ac9027bf4da3054c460a2c95
kk <a631953720@gmail.com>
2023-06-11 00:03:42 +0800

    chore: config static service

--- packages/static-service/src/main.ts
+++ packages/static-service/src/main.ts
@@ -8,13 +8,9 @@ import * as path from 'path';
 
 const app = express();
 
-app.use('/assets', express.static(path.join(__dirname, 'assets')));
+app.use('/', express.static(path.join(__dirname, 'assets')));
 
-app.get('/api', (req, res) => {
-  res.send({ message: 'Welcome to static-service!' });
-});
-
-const port = process.env.PORT || 3333;
+const port = process.env.PORT || 80;
 const server = app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}/api`);
 });
