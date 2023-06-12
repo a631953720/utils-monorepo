cfd1aa31fb7799f48c9b03c3f5d3fecce3b2defc
kk <a631953720@gmail.com>
2023-06-12 20:10:55 +0800

    feat: add retry process when init agent error

--- shared/basic/src/model/agenda/agenda.ts
+++ shared/basic/src/model/agenda/agenda.ts
@@ -1,5 +1,10 @@
 import { Agenda } from 'agenda';
 import { pgConfig } from '../constants';
+import { Loggers } from '@myorg/winston-logger';
+
+const loggers = new Loggers({
+  type: 'init agent',
+});
 
 export const agendaConfigs = {
   uri: pgConfig.mongoConfig.uri,
@@ -14,20 +19,29 @@ export const agenda = new Agenda({
 });
 
 export async function initAG() {
-  await agenda.start();
-  // agenda.define('delete old users', (job, done) => {});
+  try {
+    await agenda.start();
+    // agenda.define('delete old users', (job, done) => {});
 
-  // 危險操作，沒被定義的job都會刪除
-  // await agenda.purge();
+    // 危險操作，沒被定義的job都會刪除
+    // await agenda.purge();
 
-  // 預設最多20個工作
-  agenda.maxConcurrency(500);
+    // 預設最多20個工作
+    agenda.maxConcurrency(500);
 
-  // agenda預設lock的工作為0，等於無限制
-  // agenda.lockLimit(0);
+    // agenda預設lock的工作為0，等於無限制
+    // agenda.lockLimit(0);
 
-  // 毫秒為單位，預設lock一個工作的時間為10分鐘
-  // agenda.defaultLockLifetime(10000);
+    // 毫秒為單位，預設lock一個工作的時間為10分鐘
+    // agenda.defaultLockLifetime(10000);
 
-  agenda.name(`kk-${process.pid}`);
+    agenda.name(`kk-${process.pid}`);
+
+    // init all jobs
+  } catch {
+    setTimeout(() => {
+      loggers.error('init agent error. will retry after 5 s');
+      initAG();
+    }, 5000);
+  }
 }
